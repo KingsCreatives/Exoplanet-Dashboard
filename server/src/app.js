da6eeb810dc16536513b54516c1ffef488170ba0
kwame cody <kwamecody@gmail.com>
2024-04-02 05:41:05 +0000

    build client into server

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const  express = require('express')
+const path = require('path')
 
 const cors = require('cors')
 
@@ -13,7 +14,12 @@ app.use(
 );
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "..", 'public')))
 
 app.use(planetRouter)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "..",  'public', 'index.html'));
+});
+
 
 module.exports = app;
